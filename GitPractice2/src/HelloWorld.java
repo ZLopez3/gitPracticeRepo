15aaf401ad03e5202d9d314cd12b606b74bbdedd
zaclopez-ibanez <zaclopez-ibanez@dhcp-87-60.ram.secure.int.wireless.unca.edu>
2018-08-29 13:12:24 -0400

    Initial checkin of helloworld project

---
+++ GitPractice2/src/HelloWorld.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+
+/**
+ * @author zaclopez-ibanez
+ *
+ */
+import java.util.*;
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 * @return 
+	 */
+	public void println(String x) {
+		
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		
+		
+		System.out.println("Hello Zac how are you?");
+		String b = scan.nextLine();
+		String h = (" \n Are you sure you are " + b +" ? Enter Y or N");
+		System.out.println(h);
+		if(scan.hasNext("Y")) {
+			String g = "Great!";
+			System.out.println(g);
+		} else {
+			System.out.println("I'm sorry to hear that");
+			System.out.println("what would make you feel better?");
+			
+		}
+		scan.close();
+		
+		
+		
+	}
+
+}
